9056c5238fb7a7e07db45b9f5393af2e69babbdb
Gustavo Lemes <ggustavolemes7@gmail.com>
2026-05-26 10:11:09 -0300

    feat: aadd middleware for company and candidate

---
+++ app/Http/Middleware/EnsureRegisterCandidate.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserType;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureRegisterCandidate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if($user->user_type !== UserType::CANDIDATE)
+            abort(403);
+
+        if($user->candidate)
+            abort(403);
+
+        return $next($request);
+    }
+}
---
+++ app/Http/Middleware/EnsureRegisterCompany.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserType;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureRegisterCompany
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if($user->user_type !== UserType::COMPANY)
+            abort(403);
+
+        if($user->company)
+            abort(403);
+
+        return $next($request);
+    }
+}
